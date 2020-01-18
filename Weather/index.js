3d28dbcd10e3a7d221ac94d80c3646035dbaa243
Oliver Sun <olisun@mac.com>
2020-01-18 10:35:42 -0800

    created index.html. created index.js with logic to append buttons for cities to the DOM and to add a new button for each city

--- Weather/index.js
+++ Weather/index.js
@@ -1,11 +1,31 @@
 var currentCities = ['San Francisco', 'Seattle', 'Los Angeles', 'New York', 'Washington DC', 'Boston'];
 
-var newCity = $("#new-city-submitted");
-var cityButton = $("#city-buttons");
+var newCityButton = $("#new-city-submitted");
+var currentCitiesButtons = $("#city-buttons");
 var weatherInfo = $("#weather-info");
 
-$(document).on("click", "city-button", apiCall);
+// $(document).on("click", "city-button", apiCall);
 
 createButton = () => {
+  currentCitiesButtons.empty();
+  for (var i = 0; i < currentCities.length; i++) {
+    var cityButton = $("<button>");
+    cityButton.addClass("thisCity");
+    cityButton.attr("data-city", currentCities[i]);
+    cityButton.text(currentCities[i]);
+    currentCitiesButtons.append(cityButton);
+  };
+};
 
+createButton();
+
+newCityButton.on("click", event => {
+  event.preventDefault();
+  var newCityLabel = $("#new-city").val().trim();
+  currentCities.push(newCityLabel);
+  createButton();
+})
+
+apiCall = () => {
+  var dataAttributeCity = $(this).attr("data-city");
 }
